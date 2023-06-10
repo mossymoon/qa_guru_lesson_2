11e8203f6e2899fba77fa058a169d233df12e5df
mossymoon <stephan.sergienko@gmail.com>
2023-06-10 14:29:59 +0300

    commit changes

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
-from selene.support.shared import config
+from selene.support.shared import browser, config
 
 @pytest.fixture(autouse = True)
 def browser_window():
     config.timeout = 5
     config.window_width = 1280
     config.window_height = 720
-    config.base_url = ('https://google.com')
+    config.base_url = 'https://google.com'
     yield
+    browser.quit()
